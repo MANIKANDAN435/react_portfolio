0047913b446b55cf8056e21ec25aca3d248babee
Mani Kandan <maniikandan825@gmail.com>
2024-11-04 13:18:14 +0530

    Add resume download link

--- components/Resume.js
+++ components/Resume.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import RESUMEIMG from '../assets/resume.jpg'
+import MYRESUMEPDF from '../My_Resume/My_Resume.pdf'
 const Resume = () => {
+
+   
+
         return (
             <section className='  md:h-auto flex flex-col items-center  md:flex md:flex-row  bg-linked h-full' id='Resume'>
                 <div className=' md:h-full py-8 md:flex md:justify-end  md:w-1/2 w-[70%]'>
@@ -8,7 +12,7 @@ const Resume = () => {
                 </div>
                 <div className=' md:w-1/2 md:px-5 md:flex md:flex-col md:justify-center md:items-start text-center md:mb-5  mb-3 font-bold'>
                     <h1 className='md:text-4xl  md:mt-3 text-2xl mb-2' >RESUME</h1>
-                    <p className='md:text-2xl text-sm '>You can view my resume <a href='#'className='bg-darkblue md:px-5 md:py-1  sm:text-sm py-1 px-1 md:text-lg md:w-1/3 hover:border-2 border-pureblack'>Download</a></p> 
+                    <p className='md:text-2xl text-sm '>You can view my resume <a href={MYRESUMEPDF} download className='bg-darkblue md:px-5 md:py-1  sm:text-sm py-1 px-1 md:text-lg md:w-1/3 hover:border-2 border-pureblack'>Download</a></p> 
                 </div>
             </section>
         )
